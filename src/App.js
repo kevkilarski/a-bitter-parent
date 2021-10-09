85745d8def3693d6343143c6eee25c014fa5f5a0
lowkey <saltyndea@gmail.com>
2021-10-09 10:26:34 -0600

    Built out test api call

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+import "./App.css";
 
-function App() {  
-
+function App() {
   useEffect(() => {
     axios({
-      method: 'GET',
       headers: {
-        'x-app-id': '70c46ccc',
-        'x-app-key': 'd82034658ba4890d822655ea6603a5de',
+        "x-app-id": "70c46ccc",
+        "x-app-key": "d82034658ba4890d822655ea6603a5de",
       },
-      url: `https://api.nutritionix.com/v2/search/`,
-      dataResponse: 'json',
+      method: "GET",
+      url: `https://trackapi.nutritionix.com/v2/search/instant`,
+      dataResponse: "json",
       params: {
-        q: 'carrot'
+        query: "cheezies",
       },
-    }).then(response => {
-      console.log(response);
     })
-  }, [])
-    return (
-      <div>
+      .then((res) => {
 
-      </div>
-    )
+        console.log(res.data);
+
+        return axios({
+          headers: {
+            "Content-Type": "application/json",
+            "x-app-id": "70c46ccc",
+            "x-app-key": "d82034658ba4890d822655ea6603a5de",
+          },
+          data: {
+            query: res.data.common[0].food_name,
+          },
+          method: "POST",
+          url: `https://trackapi.nutritionix.com/v2/natural/nutrients`,
+          dataResponse: "json",
+        });
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
+
+  return <div></div>;
 }
 
-    
-    
-    export default App;
-    
-    // }) .then( (result) => {
-      // key: `d82034658ba4890d822655ea6603a5de`,
-      // AppID = `70c46ccc`,
-      
-      
+export default App;
+
+// }) .then( (result) => {
+// key: `d82034658ba4890d822655ea6603a5de`,
+// AppID = `70c46ccc`,
